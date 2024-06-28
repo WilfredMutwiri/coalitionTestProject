bf8d94b68fbde7b604689e4ce3bda9b7374ac64d
Wilfred Mutwiri <Wilfred Mutwiri>
2024-06-28 08:59:08 +0300

    add diagnostic list section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { IoIosSearch } from "react-icons/io";
 import Patients from './Patients';
 import Chartgraph from './ChartGraph';
+import respiratoryImg from '../images/respiratory rate.svg'
+import heartImg from '../images/HeartBPM.svg'
+import tempImg from '../images/temperature.svg'
 export default function Home() {
   return (
     // main home div
@@ -24,10 +27,75 @@ export default function Home() {
             <div className=' w-[200px]'>
             <h2 className='p-4 font-semibold'>Diagnosis History</h2>
         </div>
+        {/* chart section */}
         <div>
             <Chartgraph/>
         </div>
+
+        <div className='flex justify-between gap-6 mt-4'>
+            <div className='bg-blue-200 p-3 rounded-md mt-4 flex-1'>
+                <img className='w-16' src={respiratoryImg} alt='respiratoryImg'/>
+                <h2>Respiratory Rate</h2>
+                <h1 className='font-bold text-2xl'>20 bpm</h1>
+                <p className='text-sm'>Normal</p>
             </div>
+            <div className='bg-pink-200 p-3 rounded-md mt-4 flex-1'>
+                <img className='w-16' src={tempImg} alt='respiratoryImg'/>
+                <h2>Temperature</h2>
+                <h1 className='font-bold text-2xl'>98.6 F</h1>
+                <p className='text-sm'>Normal</p>
+            </div>
+            <div className='bg-pink-200 p-3 rounded-md mt-4 flex-1'>
+                <img className='w-16' src={heartImg} alt='respiratoryImg'/>
+                <h2>Heart Rate</h2>
+                <h1 className='font-bold text-2xl'>78 bpm</h1>
+                <p className='text-sm'>Lower than Average</p>
+            </div>
+        </div>
+        </div>
+        {/* list section */}
+        <div className='bg-white mt-6 rounded-md p-3'>
+            <h2 className='text-lg font-semibold'>Diagnostic List</h2>
+            <div className=''>
+                <div className='bg-gray-100 rounded-full w-full p-4 mt-4'>
+                    <ul className='flex justify-between font-semibold'>
+                        <li>Problem/Diagnosis</li>
+                        <li>Description</li>
+                        <li>Status</li>
+                    </ul>
+                </div>
+                <div className='bg-white h-28 overflow-scroll w-full p-2 flex justify-between'> 
+                <div>
+                    <ul className='flex flex-col gap-2'>
+                        <li>Hpertension</li>
+                        <li>Type 2 Diabetes</li>
+                        <li>Asthma</li>
+                        <li>Osteoarthitis</li>
+                        <li>Allergic Phintis</li>
+                        <li></li>
+                    </ul>
+                </div>
+                <div>
+                    <ul className='flex flex-col gap-2'>
+                        <li> Chronic high blood pressure</li>
+                        <li>Insulin resistance and elevated blood sugar</li>
+                        <li>Recurrent episodes of bronchial constriction</li>
+                        <li>Degenerative joint desease</li>
+                        <li>Seasonal allergies causing nasal congestion</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul className='flex flex-col gap-2'>
+                        <li>Under Observation</li>
+                        <li>Cured</li>
+                        <li>Inactive</li>
+                        <li>Untreatable</li>
+                        <li>Active</li>
+                    </ul>
+                </div>
+                </div>
+            </div>
+        </div>
         </section>
         <div className=' bg-white w-[280px]'>
             <p>
